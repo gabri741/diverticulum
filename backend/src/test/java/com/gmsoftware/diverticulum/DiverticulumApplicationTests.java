1e08117ad666b2d963b87e01db77463a99b22949
gabri741 <gabrielmartins.mca@gmail.com>
2023-10-04 08:48:05 -0300

    Spring Initializer

---
+++ backend/src/test/java/com/gmsoftware/diverticulum/DiverticulumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmsoftware.diverticulum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiverticulumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
